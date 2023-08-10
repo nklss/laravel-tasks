ef4476cb7b28cc349ddb85036aed9b006b6ed063
nklss <50378273+nklss@users.noreply.github.com>
2023-08-10 15:03:48 +0200

    commit text

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL', 'msdocs-laravel-mysql-nkl-server.postgres.database.azure.com'),
+            'host' => env('DB_HOST', 'msdocs-laravel-mysql-nkl-server.postgres.database.azure.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'msdocs-laravel-mysql-nkl-database'),
+            'username' => env('DB_USERNAME', 'hkfqjwpdmf'),
+            'password' => env('DB_PASSWORD', '0G3W651REVS6S337$'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
